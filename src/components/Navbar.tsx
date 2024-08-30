7c3da94f914c8464a9db0bdd4fe63388ecb4ac8d
gauravmandall <mandalgaurav550@gmail.com>
2024-08-30 20:27:48 +0530

    feat: inialized project
    - added prettier
    - added tailwindcss
    - added global styles
    - added layout and page components
    - added navbar component
    - theme toggle

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Rabbit } from 'lucide-react';
+import React from 'react';
+import ModeToggle from '@/components/ui/theme-button';
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center py-4">
+      <div className="flex items-center gap-2">
+        <Rabbit className="size-8" />
+        <div className="flex flex-col gap-4">
+          <span className="tracking-tighter text-3xl font-extrabold text-primary flex gap-2 items-center">
+            Geohot{' '}
+            <span className="rounded-full text-base bg-primary/10 border border-primary/50 px-2">
+              v1.0
+            </span>
+          </span>
+        </div>
+      </div>
+      <ModeToggle />
+    </nav>
+  );
+};
+
+export default Navbar;
